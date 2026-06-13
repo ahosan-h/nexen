94f2508ab5c1a6cf3c3f0a3fc5ecd3f8a4ad37c7
FAT-CAT <ahosanh896@gmail.com>
2026-06-13 22:40:46 +0600

    add auth interface

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { UserService } from './user.service';
+import type { AuthRequest } from 'src/auth/interface/auth-request.interface';
 
 @Controller('user')
 export class UserController {}
