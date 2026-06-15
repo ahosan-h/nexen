3e4215c7debbb12efa3b1cd4d97238617aceefd5
FAT-CAT <ahosanh896@gmail.com>
2026-06-15 16:24:52 +0600

    register scan schema in ScanModule

--- backend/src/scan/scan.module.ts
+++ backend/src/scan/scan.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ScanService } from './scan.service';
 import { ScanController } from './scan.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Scan, ScanSchema } from './schema/scan.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Scan.name,
+        schema: ScanSchema,
+      },
+    ]),
+  ],
+
   providers: [ScanService],
-  controllers: [ScanController]
+  controllers: [ScanController],
 })
 export class ScanModule {}
