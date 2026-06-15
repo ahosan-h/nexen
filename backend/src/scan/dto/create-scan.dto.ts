bc57b31fafe02d96a0558437b33fa48efefa7301
FAT-CAT <ahosanh896@gmail.com>
2026-06-15 20:51:54 +0600

    add scan service with create and find product methods

--- backend/src/scan/dto/create-scan.dto.ts
+++ backend/src/scan/dto/create-scan.dto.ts
@@ -1,6 +1,6 @@
 import { IsNumber, IsString } from 'class-validator';
 
-export class createScanDto {
+export class CreateScanDto {
   @IsString()
   qrcode!: string;
 
--- backend/src/scan/scan.service.ts
+++ backend/src/scan/scan.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Scan } from './schema/scan.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { CreateScanDto } from './dto/create-scan.dto';
 
 @Injectable()
-export class ScanService {}
+export class ScanService {
+  constructor(@InjectModel(Scan.name) private scanModel: Model<Scan>) {}
+
+  async findByQrCode(qrcode: string) {
+    return this.scanModel.findOne({ qrcode });
+  }
+
+  async createProduct(dto: CreateScanDto) {
+    const existProduct = await this.findByQrCode(dto.qrcode);
+
+    if (existProduct) {
+      throw new ConflictException('Product with this QR code already exists');
+    }
+
+    return this.scanModel.create(dto);
+  }
+
+  findProduct(qrcode: string) {
+    return this.findByQrCode(qrcode);
+  }
+}
