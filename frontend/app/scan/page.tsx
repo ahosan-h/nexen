7b230c80f60385b872ea65e595b2aa0b2a054147
FAT-CAT <ahosanh896@gmail.com>
2026-06-19 22:31:18 +0600

    implement qrcode/barcode scanner

--- frontend/app/scan/page.tsx
+++ frontend/app/scan/page.tsx
@@ -1,4 +1,10 @@
-import BarScanner from "@/components/BarScanner";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const BarScanner = dynamic(() => import("@/components/BarScanner"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
--- frontend/components/BarScanner.tsx
+++ frontend/components/BarScanner.tsx
@@ -1,3 +1,41 @@
+// components/QrScanner.jsx
+"use client";
+
+import { useEffect } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
+
 export default function BarScanner() {
-  return <h1>hello hehe</h1>;
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner(
+      "reader",
+      {
+        fps: 10,
+        qrbox: {
+          width: 250,
+          height: 250,
+        },
+      },
+      false,
+    );
+
+    const onScanSuccess = (decodedText: string, decodedResult: string) => {
+      console.log("Code matched =", decodedText, decodedResult);
+
+      // Optional: stop scanning after successful scan
+      scanner.clear().catch(console.error);
+    };
+
+    const onScanFailure = (error) => {
+      // Usually ignore scan failures
+      console.warn("Code scan error =", error);
+    };
+
+    scanner.render(onScanSuccess, onScanFailure);
+
+    return () => {
+      scanner.clear().catch(console.error);
+    };
+  }, []);
+
+  return <div id="reader" />;
 }
