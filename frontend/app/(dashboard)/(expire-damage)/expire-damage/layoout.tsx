725290da41848f017ecbcf7f83da34ff4138c0bf
FAT-CAT <ahosanh896@gmail.com>
2026-07-01 00:44:04 +0600

    add damage and expired report tabs with navigation cards

---
+++ frontend/app/(dashboard)/(expire-damage)/expire-damage/layoout.tsx
@@ -0,0 +1,14 @@
+// app/layout.tsx
+export default function DamageExpire({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/app/(dashboard)/(expire-damage)/expire-damage/page.tsx
@@ -0,0 +1,55 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ClockAlert, FileWarningIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function DamageExpire() {
+  return (
+    <div>
+      <Tabs
+        className="w-full flex items-center justify-center"
+        defaultValue="report"
+      >
+        <TabsList variant="line">
+          <TabsTrigger value="report" className="text-2xl font-bold">
+            Report
+          </TabsTrigger>
+          <TabsTrigger value="pending" className="text-2xl font-bold">
+            Pending
+          </TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="report" className="mt-10">
+          <div className="flex flex-col gap-4">
+            <Link href="/damage">
+              <Card className="rounded-xl border bg-card hover:border-primary hover:bg-accent/30 transition-all cursor-pointer">
+                <CardContent className="flex w-full px-8 sm:w-sm md:w-md h-20 flex-col items-center justify-center gap-2">
+                  <div className="flex gap-1">
+                    <FileWarningIcon />
+                    <p className="text-lg font-medium">Damage Report</p>
+                  </div>
+                  <p className="text-sm text-muted-foreground">
+                    Submit a damage report →
+                  </p>
+                </CardContent>
+              </Card>
+            </Link>
+            <Link href="/expire">
+              <Card className="rounded--xl border bg-card hover:border-primary hover:bg-accent/30 transition-all cursor-pointer">
+                <CardContent className="flex w-full px-8 sm:w-sm md:w-md h-20 flex-col items-center justify-center gap-2">
+                  <div className="flex gap-1">
+                    <ClockAlert />
+                    <p className="text-lg font-medium">Expired Report</p>
+                  </div>
+                  <p className="text-sm text-muted-foreground">
+                    Submit an expired report →
+                  </p>
+                </CardContent>
+              </Card>
+            </Link>
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
