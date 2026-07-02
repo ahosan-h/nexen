5b2a4a124779450fb85f16b7a1ff55c6d3419a22
FAT-CAT <ahosanh896@gmail.com>
2026-07-03 00:37:48 +0600

    add  user service

--- frontend/service/user.service.ts
+++ frontend/service/user.service.ts
@@ -1,1 +1,16 @@
-export const UserService = {};
+import { apiFetch } from "@/lib/api";
+import { createUserDto, User } from "@/types/user";
+
+export const UserService = {
+  create: (data: createUserDto, token: string) =>
+    apiFetch<User>("/user", {
+      method: "POST",
+      token,
+      body: data,
+    }),
+
+  find: (token: string) =>
+    apiFetch<User>("/user/me", {
+      token,
+    }),
+};
